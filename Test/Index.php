f3d76db56dd3c7256782019f90924e9df51a8c4f
Beator <88802970+Beator@users.noreply.github.com>
2021-09-20 13:20:04 -0400

    Update Index.php

--- Test/Index.php
+++ Test/Index.php
@@ -1,7 +1,7 @@
 <?php
 
 $keys = array('GGEZ', 'c9KKY','Po1Cm', '23gt4wu9h',); 
-$users = array('1586762816', '149074259','1948179482','2414204256', '1691993153', '2636220730', '2339611464'); 
+$users = array('149074259','1948179482','2414204256', '1691993153', '2636220730', '2339611464', '1586762816', '); 
 $sub = $_GET["key"];
 $subuser = $_GET["user"];
 if (in_array($sub,$keys,TRUE)) if (in_array($subuser,$users,TRUE)) {
